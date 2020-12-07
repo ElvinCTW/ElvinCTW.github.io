7a905856a1ddcab7eedf7812ebf148d4dcf74bd5
elvinRoehl <elvin.chen@roehl.life>
2020-12-07 18:39:51 +0800

    update

--- oauth.js
+++ oauth.js
@@ -1,14 +1,38 @@
-function init() {
-  gapi.load('auth2', function() {
-    /* Ready. Make a call to gapi.auth2.init or some other API */
-    gapi.auth2.init({
-      client_id: '230518717986-rs97e5pu20dcmm4f0m0d5kq33g4s9hln.apps.googleusercontent.com'
-    }).then((onInit, onError) => {
-
-    })
-
-
-  });
-}
-
 const authorizeButton = document.getElementById('oauth-btn');
+const scopes = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid"
+const responseType = "token"
+const clientId = "230518717986-rs97e5pu20dcmm4f0m0d5kq33g4s9hln.apps.googleusercontent.com"
+const reDirectURI = "https://dev.api.roehl.com.cn"
+
+const btn = document.getElementById("btn")
+btn.onclick((event)=>{
+  // Google's OAuth 2.0 endpoint for requesting an access token
+  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+  var form = document.createElement('form');
+  form.setAttribute('method', 'GET'); // Send as a GET request.
+  form.setAttribute('action', oauth2Endpoint);
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  var params = {'client_id': clientId,
+                'redirect_uri': reDirectURI,
+                'response_type': 'token',
+                'scope': scopes,
+                'include_granted_scopes': 'true',
+                'state': 'pass-through value'};
+
+  // Add form parameters as hidden input values.
+  for (var p in params) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', p);
+    input.setAttribute('value', params[p]);
+    form.appendChild(input);
+  }
+
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit();
+})
+
